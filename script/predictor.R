baefc89f3fa4dbaa40104e47b2e2d7a4e47d7027
Mattia Vio <theus84@gmail.com>
2018-05-07 08:33:24 +0200

    added functionality to predictor script

--- script/predictor.R
+++ script/predictor.R
@@ -1,3 +1,23 @@
+# Load packages
+library('ggplot2') # visualization
+library('ggthemes') # visualization
+library('scales') # visualization
+library('dplyr') # data manipulation
+library('mice') # imputation
+library('randomForest') # classification algorithm
+
 
 train = read.csv("input/train.csv",header=TRUE,stringsAsFactors = FALSE)
 test = read.csv("input/test.csv",header=TRUE,stringsAsFactors = FALSE)
+full  <- bind_rows(train, test) # bind training & test data
+
+
+data = train 
+
+imp <- mice(data, m = 5)
+# m = 1 specifies a single imputation, standard would be m = 5 for multiple imputation
+# The imputation method could be specified with 'method = ' - standard is pmm
+# The predictor matrix could be specified with 'predictorMatrix'
+
+# Completed data
+data_imp <- complete(imp)
