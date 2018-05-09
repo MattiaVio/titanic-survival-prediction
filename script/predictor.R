deb8cf8db2a835a459c5664ef4cf46a6b3ba7ad3
Mattia Vio <theus84@gmail.com>
2018-05-09 07:48:06 +0200

    added surname to the full table

--- script/predictor.R
+++ script/predictor.R
@@ -12,6 +12,13 @@ test = read.csv("input/test.csv",header=TRUE,stringsAsFactors = FALSE)
 full  <- bind_rows(train, test) # bind training & test data
 
 
+# Grab title from passenger names
+full$Title <- gsub('(.*, )|(\\..*)', '', full$Name)
+# Show title counts by sex
+table(full$Sex, full$Title)
+
+full$Surname <- gsub( ",.*$", "", full$Name )
+
 data = train 
 
 imp <- mice(data, m = 5)
@@ -25,6 +32,6 @@ data_imp <- complete(imp)
 # Plot age distributions
 par(mfrow=c(1,2))
 hist(train$Age, freq=F, main='Age: Original Data', 
-     col='darkgreen', ylim=c(0,0.04))
+     col='darkblue', ylim=c(0,0.04))
 hist(data_imp$Age, freq=F, main='Age: MICE Output', 
-     col='lightgreen', ylim=c(0,0.04))
+     col='lightblue', ylim=c(0,0.04))
